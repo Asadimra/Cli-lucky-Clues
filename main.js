3e3c6809faf1e9abe896fc7417da1c29f66fff2c
Asadimra <asadimran031@gmail.com>
2024-03-23 18:00:02 +0500

    uploading the code

---
+++ main.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const random_number = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([{
+        name: "user_guessed_number",
+        type: "number",
+        message: "Please Guess a Number between 1-6 : ",
+    }]);
+if (answer.user_guessed_number === random_number) {
+    console.log("conguralation you gussed  a right number: ");
+}
+else {
+    console.log("Oh Know you guessed wrong number: ");
+    console.log("GAME OVER");
+}
